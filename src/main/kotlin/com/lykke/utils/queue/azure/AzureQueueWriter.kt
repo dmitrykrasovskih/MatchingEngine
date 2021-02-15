ef78c9be82de80ce52ef18c3d8e26d415fe658f8
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-02-15 02:21:46 +0200

    - disable azure logging

--- src/main/kotlin/com/lykke/utils/queue/azure/AzureQueueWriter.kt
+++ src/main/kotlin/com/lykke/utils/queue/azure/AzureQueueWriter.kt
@@ -2,9 +2,6 @@ package com.lykke.utils.queue.azure
 
 import com.lykke.utils.logging.ThrottlingLogger
 import com.lykke.utils.queue.QueueWriter
-import com.microsoft.azure.storage.CloudStorageAccount
-import com.microsoft.azure.storage.queue.CloudQueue
-import com.microsoft.azure.storage.queue.CloudQueueMessage
 
 internal class AzureQueueWriter(queueConnectionString: String, queueName: String) : QueueWriter {
 
@@ -12,22 +9,22 @@ internal class AzureQueueWriter(queueConnectionString: String, queueName: String
         private val LOGGER = ThrottlingLogger.getLogger(AzureQueueWriter::class.java.name)
     }
 
-    private val outQueue: CloudQueue
+//    private val outQueue: CloudQueue
 
     override fun write(data: String) {
-        try {
-            outQueue.addMessage(CloudQueueMessage(data))
-        } catch (e: Exception) {
-            LOGGER.error("Unable to enqueue message to azure queue: $data", e)
-        }
+//        try {
+//            outQueue.addMessage(CloudQueueMessage(data))
+//        } catch (e: Exception) {
+//            LOGGER.error("Unable to enqueue message to azure queue: $data", e)
+//        }
     }
 
     init {
-        val storageAccount = CloudStorageAccount.parse(queueConnectionString)
-        val queueClient = storageAccount.createCloudQueueClient()
-        outQueue = queueClient.getQueueReference(queueName)
-        if (!outQueue.exists()) {
-            throw Exception("Azure $queueName queue does not exists")
-        }
+//        val storageAccount = CloudStorageAccount.parse(queueConnectionString)
+//        val queueClient = storageAccount.createCloudQueueClient()
+//        outQueue = queueClient.getQueueReference(queueName)
+//        if (!outQueue.exists()) {
+//            throw Exception("Azure $queueName queue does not exists")
+//        }
     }
 }
