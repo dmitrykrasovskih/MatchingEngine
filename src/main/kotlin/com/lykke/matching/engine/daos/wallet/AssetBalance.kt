f8325e2886257f6193479ba3c547689dfad91a0d
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-04-18 02:03:32 +0300

    fix

--- src/main/kotlin/com/lykke/matching/engine/daos/wallet/AssetBalance.kt
+++ src/main/kotlin/com/lykke/matching/engine/daos/wallet/AssetBalance.kt
@@ -3,16 +3,17 @@ package com.lykke.matching.engine.daos.wallet
 import org.nustaq.serialization.annotations.Version
 import java.io.Serializable
 import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
 
 class AssetBalance(
     val clientId: String,
     val asset: String,
-    var balance: BigDecimal = BigDecimal.ZERO,
-    var reserved: BigDecimal = BigDecimal.ZERO,
-    @Version(2) var reservedForSwap: BigDecimal = BigDecimal.ZERO,
+    var balance: BigDecimal = ZERO,
+    var reserved: BigDecimal = ZERO,
+    @Version(2) var reservedForSwap: BigDecimal = ZERO,
     @Version(1) var brokerId: String = "",
     @Version(1) var accountId: String = "",
     @Version(1) var version: Long = 0
 ) : Serializable {
-    fun getTotalReserved(): BigDecimal = reserved + reservedForSwap
+    fun getTotalReserved(): BigDecimal = (reserved ?: ZERO) + (reservedForSwap ?: ZERO)
 }
